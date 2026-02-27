d8d5d2ac4c4c19ce86efea0c181751912106329a
sreeshaashanmugavel07-blip <sreeshaashanmugavel07@gmail.com>
2026-02-27 15:07:48 +0530

    UC1: Add welcome message module

--- src/palindromeCheckerApp.java
+++ src/palindromeCheckerApp.java
@@ -1,3 +1,38 @@
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class palindromeCheckerApp {
 
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear  = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input String : " + input);
+
+        if (isPalindrome) {
+            System.out.println("Result : The string is a Palindrome.");
+        } else {
+            System.out.println("Result : The string is NOT a Palindrome.");
+        }
+
+        System.out.println("Program executed successfully.");
+    }
 }
